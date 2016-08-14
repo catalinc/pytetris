71e8ab1e9ad68073dc9f15accd565da8bcd4d11d
Catalin Cristu <catalin.cristu@gmail.com>
2016-08-14 23:20:23 +0300

    implement pause/resume & game over/restart

--- game.py
+++ game.py
@@ -3,18 +3,20 @@ import sys
 
 import pygame
 
-from board import SHAPE_I, SHAPE_O, ALL_SHAPES, DIRECTION_DOWN, DIRECTION_LEFT, DIRECTION_RIGHT, Piece, Board
+import board
 
 
 class State(object):
     def __init__(self, rows, cols):
-        self.board = Board(rows, cols)
+        self.board = board.Board(rows, cols)
         self.score = 0
         self.piece = None
         self.shapes = []
         self.elapsed = 0
         self.level = -1
         self.speed = 0
+        self.paused = False
+        self.game_over = False
         self.next_level()
         self.next_piece()
 
@@ -22,13 +24,13 @@ class State(object):
         shape = self.next_shape()
         self.shapes.remove(shape)
         row = col = 0
-        if shape in (SHAPE_I, SHAPE_O):
+        if shape in (board.SHAPE_I, board.SHAPE_O):
             col = self.board.cols / 2
-        self.piece = Piece(shape, row, col)
+        self.piece = board.Piece(shape, row, col)
 
     def next_shape(self):
         if not self.shapes:
-            self.shapes = list(ALL_SHAPES)
+            self.shapes = list(board.ALL_SHAPES)
             random.shuffle(self.shapes)
         return self.shapes[0]
 
@@ -38,17 +40,19 @@ class State(object):
 
     def drop_piece(self):
         while not self.board.has_landed(self.piece):
-            self.move_piece(DIRECTION_DOWN)
+            self.move_piece(board.DIRECTION_DOWN)
 
     def rotate_piece(self, ccw=False):
         if self.board.can_rotate(self.piece, ccw):
             self.piece.rotate(ccw)
 
     def update(self, dt):
+        if self.game_over or self.paused:
+            return
         self.elapsed += dt
         if self.elapsed >= self.speed:
             if not self.board.has_landed(self.piece):
-                self.move_piece(DIRECTION_DOWN)
+                self.move_piece(board.DIRECTION_DOWN)
                 self.elapsed = 0
         if self.board.has_landed(self.piece):
             self.board.land(self.piece)
@@ -58,11 +62,9 @@ class State(object):
             self.update_score(lines)
             if self.score % 1000 == 0 and self.level < 9:
                 self.next_level()
-
-    def game_over(self):
         for c in range(self.board.cols):
             if self.board.block_at(0, c):
-                return True
+                self.game_over = True
 
     POINTS_PER_LINES = {1: 40, 2: 100, 3: 300, 4: 1200}
 
@@ -73,12 +75,19 @@ class State(object):
         self.level += 1
         self.speed = 1000 - self.level * 100
 
+    @property
+    def running(self):
+        return not (self.paused or self.game_over)
+
 
 class Game(object):
+    ROWS = 20
+    COLS = 10
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.state = State(20, 10)
+        self.state = State(self.ROWS, self.COLS)
         self.block_size = height / self.state.board.rows
         self.board_width = self.block_size * self.state.board.cols
         self.board_height = self.block_size * self.state.board.rows
@@ -109,7 +118,7 @@ class Game(object):
         x = self.board_width + (self.width - self.board_width - self.block_size) / 2
         row = y / self.block_size
         col = x / self.block_size - 1
-        piece = Piece(self.state.next_shape(), row, col)
+        piece = board.Piece(self.state.next_shape(), row, col)
         pygame.draw.rect(self.screen, piece.shape.color,
                          (x - self.block_size * 2, y - self.block_size / 2,
                           self.block_size * 5, self.block_size * 4), 1)
@@ -127,21 +136,33 @@ class Game(object):
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    self.state.move_piece(DIRECTION_DOWN)
-                if event.key == pygame.K_LEFT:
-                    self.state.move_piece(DIRECTION_LEFT)
-                if event.key == pygame.K_RIGHT:
-                    self.state.move_piece(DIRECTION_RIGHT)
-                if event.key == pygame.K_x:
-                    self.state.rotate_piece()
-                if event.key == pygame.K_z:
-                    self.state.rotate_piece(True)
-                if event.key == pygame.K_SPACE:
-                    self.state.drop_piece()
+                if self.state.paused:
+                    if event.key == pygame.K_r:
+                        self.state.paused = False
+                if self.state.game_over:
+                    if event.key == pygame.K_r:
+                        self.state = State(self.ROWS, self.COLS)
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+                if self.state.running:
+                    if event.key == pygame.K_DOWN:
+                        self.state.move_piece(board.DIRECTION_DOWN)
+                    if event.key == pygame.K_LEFT:
+                        self.state.move_piece(board.DIRECTION_LEFT)
+                    if event.key == pygame.K_RIGHT:
+                        self.state.move_piece(board.DIRECTION_RIGHT)
+                    if event.key == pygame.K_x:
+                        self.state.rotate_piece()
+                    if event.key == pygame.K_z:
+                        self.state.rotate_piece(True)
+                    if event.key == pygame.K_SPACE:
+                        self.state.drop_piece()
+                    if event.key == pygame.K_p:
+                        self.state.paused = True
 
     def update_state(self):
-        self.state.update(self.clock.get_time())
+        if self.state.running:
+            self.state.update(self.clock.get_time())
 
     def render(self):
         self.draw_background()
@@ -152,6 +173,12 @@ class Game(object):
         self.draw_text('%d' % self.state.score, self.block_size * 4)
         self.draw_text('Next', self.block_size * 5)
         self.draw_next_piece(self.block_size * 6)
+        if self.state.paused:
+            self.draw_text('Game paused', self.block_size * 10)
+            self.draw_text('Press R to resume', self.block_size * 11)
+        if self.state.game_over:
+            self.draw_text('Game over', self.block_size * 10)
+            self.draw_text('Press R to restart', self.block_size * 11)
         pygame.display.flip()
         self.clock.tick(60)
 
